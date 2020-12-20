cf30325a1db9af6c1b304ff74b3e898b11b1e690
ncentola <centola.nick@gmail.com>
2020-12-20 11:52:34 -0600

    react posts messages here and api published them to queue

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -3,14 +3,15 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
+# from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# cors = CORS(app, resources={r"*": {"origins": "*"}})
+
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-
-migrate = Migrate(app, db)
 
 api = Api(app)
 
--- api/app/util_routes.py
+++ api/app/util_routes.py
@@ -1,7 +1,11 @@
 from .resources import ProductResource, TransactionListResource
+from .publisher import Publisher
 from flask import request
 from app import app, api, db
 import pandas as pd
+import json
+
+pub = Publisher()
 
 @app.route('/upc_lookup', methods=['POST'])
 def upc_lookup():
@@ -16,3 +20,9 @@ def get_product_quantities():
 
     data = pd.read_sql('select * from product_quantities_vw', db.engine)
     return data.to_json(orient='records')
+
+@app.route('/queue_message', methods=['POST'])
+def queue_message():
+
+    pub.publish(routing_key='scanner', message=json.dumps(request.json))
+    return {'status': 'success'}
